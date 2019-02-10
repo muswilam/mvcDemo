86ae5c3c62cfa9853daabca5f8c77df25b04d1dc
muswilam <jackswilam@gmail.com>
2019-02-10 17:58:40 +0200

    add HomeController to view list of Contries

---
+++ mvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewData["Contries"] = new List<string>()
+            {
+                "Egypt",
+                "England",
+                "Germany",
+                "Catalunia",
+                "France"
+            };
+            return View();
+        }
+
+    }
+}
