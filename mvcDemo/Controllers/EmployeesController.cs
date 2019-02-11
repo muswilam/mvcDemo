c10126f9b21695d3b3cfc7bea3f1649eee994183
muswilam <jackswilam@gmail.com>
2019-02-11 16:38:34 +0200

    add a new page views list of employees names in actionlinks

--- mvcDemo/Controllers/EmployeesController.cs
+++ mvcDemo/Controllers/EmployeesController.cs
@@ -11,14 +11,23 @@ namespace mvcDemo.Controllers
     {
         //
         // GET: /Employees/
+        public  ActionResult Index()
+        {
+            // view list of employees from empClass
+            EmployeeContext employeeContext = new EmployeeContext();
+            List<Employee> employees = employeeContext.employee.ToList();
+            return View(employees);
+        }
 
         public ActionResult Details(int id) // should name this parmeter with 'id' because of default rout or use (?ur_parmeter=num)
         {
             // make instance from classContext then retrieve just one row by 'single' 
             EmployeeContext employeeContext = new EmployeeContext();
-            Employee employees = employeeContext.employee.Single(emp => emp.id == id);
-            return View(employees);
+            Employee employee = employeeContext.employee.Single(emp => emp.id == id);
+            return View(employee);
         }
 
     }
+
+
 }
